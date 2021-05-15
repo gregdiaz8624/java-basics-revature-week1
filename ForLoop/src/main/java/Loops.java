d65781534b289e7255eafa282fda874090a1538c
Gregory Diaz <gregdiaz8624@yahoo.com>
2021-05-15 18:29:55 -0400

    Added a few labs, up to for loops

---
+++ ForLoop/src/main/java/Loops.java
@@ -0,0 +1,19 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+		
+			System.out.println(i);
+			
+		}
+	}
+	
+}
+
+//..One important note is that we used the variable,
+//i (which we defined in our declaration statement) 
+//inside of our loop. Because the increment statement
+//increases the value by one each time, we are effectively
+//counting from 1 to 10 and storing that value in the variable 
+//i during each iteration of the loop.
