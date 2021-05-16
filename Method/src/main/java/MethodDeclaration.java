00aa046d42185f70c2bab7178f12690035f06fd6
Gregory Diaz <gregdiaz8624@yahoo.com>
2021-05-15 21:06:38 -0400

    finished all day 4 labs for week 1

---
+++ Method/src/main/java/MethodDeclaration.java
@@ -0,0 +1,21 @@
+
+public class MethodDeclaration {
+
+	public static void main(String[] args) {
+		
+		
+		MethodDeclaration md = new MethodDeclaration();
+		
+		
+		md.talk();
+		md.talk();
+		md.talk();
+		
+	}
+	
+	public void talk() {
+		
+		System.out.println("Inside the talk method");
+	}
+	
+}
---
+++ MethodReturnType/src/main/java/ReturnTypes.java
@@ -0,0 +1,23 @@
+
+public class ReturnTypes {
+	
+	public static void main(String[] args) {
+		
+		ReturnTypes rt = new ReturnTypes();
+		
+		rt.returnNothing();
+		
+		boolean b = rt.returnBoolean();
+		
+		System.out.println("The value of b is " + b);
+	}
+
+	public void returnNothing() {
+		System.out.println("Inside of void method");
+	}
+	
+	
+	public boolean returnBoolean() {
+		return true;
+	}
+}
